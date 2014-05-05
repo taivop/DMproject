24172b2edaf0502d40b6f373fd0899519ee75074
taivop <taivo.pungas@gmail.com>
2014-05-06 00:34:04 +0300

    Read diagnoses into a dictionary.

--- src/code_examples.py
+++
@@ -1,7 +0,0 @@
-data_file = '../data/age_predict_data.txt'
-
-import numpy as np
-data = np.loadtxt(data_file,delimiter='\t', skiprows=161439, dtype={'names': ('gender', 'age', 'diagnoses', 'prescriptions'),'formats': ('S1', 'i3', 'object', 'object')})
-# loaded data starting from the 161439-th row, loaded 'diagnoses' and 'prescriptions' fields as Python string objects, not numpy string fields
-
-print(data)
---
+++ src/diagnoses.py
@@ -0,0 +1,44 @@
+import json
+
+
+def preprocessDiagnoses():
+
+    # read file
+    filename = '../data/rhk.txt'
+    f = open(filename, encoding='utf-8')
+    lines = f.readlines()
+    f.close()
+
+    # initialise dictionary
+    diagnoses = dict()
+
+    # go through every diagnosis
+    for line in lines:
+        stringlist = line.strip('').split()
+
+        # if it is a general diagnosis (code doesn't contain a '.'), add it to dictionary
+        if stringlist[0].find('.') == -1:
+            # add to dictionary
+            diagnoses[stringlist[0]] = ' '.join(stringlist[1:])
+
+    # save the dictionary into a file
+    filename = '..\data\diagnoses.dict'
+    f = open(filename,'w')
+    print(json.dumps(diagnoses))
+    f.write(json.dumps(diagnoses))     # json format
+    f.close()
+
+    return
+
+
+def getDiagnosesDictionary():
+    # read dict from file
+    filename = '..\data\diagnoses.dict'
+    f = open(filename)
+    line = f.readline()
+    f.close()
+
+    # turn string into python dict
+    diagnoses = json.loads(line)
+
+    return diagnoses

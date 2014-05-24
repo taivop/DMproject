0f2c0c9a861dc514c86f435669f3e8fa7fbbfaf4
taivop <taivo.pungas@gmail.com>
2014-05-25 02:26:03 +0300

    Evaluator now loads test data.

--- src/analysis/example.py
+++ src/analysis/example.py
@@ -6,4 +6,5 @@ handler = DataHandler()
 genders, ages, diagnoses = handler.getTrainingData()
 
 # get test set
+# although this shouldn't be necessary since the Evaluator class loads test data and calculates necessary measures
 genders_test, ages_test, diagnoses_test = handler.getTestData()
--- src/helpers/evaluator.py
+++ src/helpers/evaluator.py
@@ -1,12 +1,18 @@
 import numpy as np
+from helpers.datahandler import DataHandler
 
 class Evaluator():
 
     test_genders = np.asarray([[0],[1],[1]])
 
+    genders_test = None
+    ages_test = None
+    diagnoses_test = None
+
     def __init__(self):
-        0
         # load test data
+        handler = DataHandler()
+        self.genders_test, self.ages_test, self.diagnoses_test = handler.getTestData()
 
     def genders_fscore(self, predictions):
         foo = 0

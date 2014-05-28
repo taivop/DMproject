d38bde200ee490e3cd50eb86385f8f68384e9d87
taivop <taivo.pungas@gmail.com>
2014-05-28 15:40:49 +0300

    Figures, poster, distributions.

--- src/test/fig_distributions.py
+++ src/test/fig_distributions.py
@@ -17,7 +17,7 @@ print("Women: {0}".format(sum(genders == 0)))
 # --- Ages distribution ---
 print("\nAges distribution")
 # create histogram data for age distribution
-bins = range(0,101,5)
+bins = range(0,101,1)#range(0,101,5)
 hist, bins = np.histogram(ages, bins=bins)
 print("Ages histogram values:")
 [print("{0}".format(h)) for h in hist]
@@ -37,7 +37,7 @@ plt.show()
 # --- Distribution of numbers of diagnoses ---
 print("\nDiagnoses distribution")
 summed = np.sum(diagnoses, axis=1)
-bins = range(0,59,2)
+bins = range(0,59,1)
 hist, bins = np.histogram(summed, bins=bins)
 print("Diagnosis histogram values:")
 [print("{0}".format(h)) for h in hist]
--- src/test/testAges.py
+++ src/test/testAges.py
@@ -2,21 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from helpers.age import Age
 
-ages = np.load('../data/ages_train.npy')
+ages = np.load('../../data/ages_train.npy')
 
 # print min, max
 print(np.min(ages))
 print(np.max(ages))
 
 # create histogram data for age distribution
-hist, bins = np.histogram(ages, bins=12)
+binEdges = [0, 20, 30, 40, 50, 60, 70, 80, 100]
+hist, bins = np.histogram(ages, bins=binEdges)
 print(hist)
 
 # plot histogram
 width = 0.7 * (bins[1] - bins[0])
 center = (bins[:-1] + bins[1:]) / 2
 plt.bar(center, hist, align='center', width=width)
-#plt.show()
+plt.show()
 
 
 a = Age()
